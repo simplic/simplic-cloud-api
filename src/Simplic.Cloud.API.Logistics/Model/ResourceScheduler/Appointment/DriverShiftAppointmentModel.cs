0590df3dd0e0faee3f08ba46524bc6ccd513b3cf
Benedikt Eggers <be@spiegelburg.net>
2020-06-02 14:53:21 +0200

    Add aggregation

--- src/Simplic.Cloud.API.Logistics/Model/ResourceScheduler/Appointment/DriverShiftAppointmentModel.cs
+++ src/Simplic.Cloud.API.Logistics/Model/ResourceScheduler/Appointment/DriverShiftAppointmentModel.cs
@@ -6,6 +6,22 @@ namespace Simplic.Cloud.API.Logistics
 {
     public class DriverShiftAppointmentModel : AppointmentBaseModel
     {
+        /// <summary>
+        /// Gets or sets the aggregated sales price
+        /// </summary>
+        public double AggregatedSalesPrices
+        {
+            get;
+            set;
+        }
 
+        /// <summary>
+        /// Gets or sets the aggregated margin
+        /// </summary>
+        public double AggregatedMargin
+        {
+            get;
+            set;
+        }
     }
 }
