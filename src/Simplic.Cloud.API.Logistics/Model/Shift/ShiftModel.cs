f1676750133cd65c7d0522ad3377bae94c0b2963
Benedikt Eggers <be@spiegelburg.net>
2020-06-07 13:59:11 +0200

    Change driver shift

--- src/Simplic.Cloud.API.Logistics/Model/Shift/ShiftModel.cs
+++ src/Simplic.Cloud.API.Logistics/Model/Shift/ShiftModel.cs
@@ -10,8 +10,11 @@ namespace Simplic.Cloud.API.Logistics
     {
         public Guid Id { get; set; }
         public Guid? DriverId { get; set; }
+        public IList<Guid> DefaultVehicleIds { get; set; } = new List<Guid>();
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
+        public DateTime? ActualStartDate { get; set; }
+        public DateTime? ActualEndDate { get; set; }
         public IList<Guid> TourIds { get; set; }
     }
 }
